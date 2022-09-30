8d7f48f6a95a695635c0db3683524ebc8944bf53
Eduard <edpufa@inspalamos.cat>
2022-09-30 20:00:57 +0200

    Update MainActivity.java

--- EduPujadasActCinc/app/src/main/java/edu/pujadas/MainActivity.java
+++ EduPujadasActCinc/app/src/main/java/edu/pujadas/MainActivity.java
@@ -3,29 +3,32 @@ package edu.pujadas;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 
 /**
  * Dades mock
  *
- * usuari 1
- *
- * user: admin
- * pass : 1234
- *
- * user 2
- *
- * user: user
- * pass: 1234
+ * user: admin password 1234  ||||| usuari: user pass 1234 
  */
 public class MainActivity extends AppCompatActivity
 {
 
-
+    EditText userName;
+    EditText paswordInfo;
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void cheackUser(View vista)
+    {
+
+    }
+
+
+
 }
