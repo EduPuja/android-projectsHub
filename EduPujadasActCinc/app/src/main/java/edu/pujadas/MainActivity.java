3c974c1b9ec20787abeb7bd8a4e6d770e47b5941
Eduard <edpufa@inspalamos.cat>
2022-09-30 19:54:07 +0200

    update MainActivity

--- EduPujadasActCinc/app/src/main/java/edu/pujadas/MainActivity.java
+++ EduPujadasActCinc/app/src/main/java/edu/pujadas/MainActivity.java
@@ -4,8 +4,23 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+
+/**
+ * Dades mock
+ *
+ * usuari 1
+ *
+ * user: admin
+ * pass : 1234
+ *
+ * user 2
+ *
+ * user: user
+ * pass: 1234
+ */
 public class MainActivity extends AppCompatActivity
 {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
