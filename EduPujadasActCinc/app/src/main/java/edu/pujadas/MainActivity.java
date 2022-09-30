f8b317fbf5d9047be341632221290e925780813d
Eduard <edpufa@inspalamos.cat>
2022-09-30 20:04:17 +0200

    Update MainActitiy.java
    Add the method cheackUser that can can validate the user

--- EduPujadasActCinc/app/src/main/java/edu/pujadas/MainActivity.java
+++ EduPujadasActCinc/app/src/main/java/edu/pujadas/MainActivity.java
@@ -10,7 +10,7 @@ import android.widget.EditText;
 /**
  * Dades mock
  *
- * user: admin password 1234  ||||| usuari: user pass 1234 
+ * user: admin password 1234  ||||| usuari: user pass 1234
  */
 public class MainActivity extends AppCompatActivity
 {
@@ -22,8 +22,19 @@ public class MainActivity extends AppCompatActivity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // busca una id que es digui username
+        userName = findViewById(R.id.userName);
+        // busco per la id la password
+        paswordInfo = findViewById(R.id.password);
     }
 
+    /**
+     * Metode utilizat onclick buton
+     *
+     *this method validates if the user that you entry is a admin or a user
+     * @param vista
+     */
     public void cheackUser(View vista)
     {
 
