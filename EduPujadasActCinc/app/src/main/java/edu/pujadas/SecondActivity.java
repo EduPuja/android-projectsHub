98bdb3b2ed09d2c79797d66da5914d925484e1a8
Eduard <edpufa@inspalamos.cat>
2022-10-03 20:19:12 +0200

    Update SecondActivity

--- EduPujadasActCinc/app/src/main/java/edu/pujadas/SecondActivity.java
+++ EduPujadasActCinc/app/src/main/java/edu/pujadas/SecondActivity.java
@@ -3,14 +3,19 @@ package edu.pujadas;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.snackbar.Snackbar;
 
 public class SecondActivity extends AppCompatActivity
 {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState)
+    //@Override
+    protected void onCreate(Bundle savedInstanceState, View vista)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+        Snackbar mySnackbar = Snackbar.make(vista, "HOla", 2);
+        mySnackbar.show();;
     }
 }
