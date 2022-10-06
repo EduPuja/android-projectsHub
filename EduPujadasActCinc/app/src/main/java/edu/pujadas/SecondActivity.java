c057356983c4ff614b49e6eac1e5d45980ae0926
Eduard <edpufa@inspalamos.cat>
2022-10-06 15:53:43 +0200

    delete buton

--- EduPujadasActCinc/app/src/main/java/edu/pujadas/SecondActivity.java
+++ EduPujadasActCinc/app/src/main/java/edu/pujadas/SecondActivity.java
@@ -4,19 +4,20 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 import com.google.android.material.snackbar.Snackbar;
 
 public class SecondActivity extends AppCompatActivity
 {
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
-        /*Snackbar mySnackbar = Snackbar.make(findViewById(R.id.layout), "Benvingut Admin", 1000);
-        mySnackbar.show();*/
+
     }
 
     public void showSnackbar(View vista)
