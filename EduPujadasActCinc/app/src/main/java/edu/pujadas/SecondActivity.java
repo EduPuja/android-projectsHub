ada244e6566c37d862809c93119a12c354103d24
Eduard <edpufa@inspalamos.cat>
2022-10-06 16:58:24 +0200

    Update SecondActivity.java
    Set the bundle extras and add the tilte

--- EduPujadasActCinc/app/src/main/java/edu/pujadas/SecondActivity.java
+++ EduPujadasActCinc/app/src/main/java/edu/pujadas/SecondActivity.java
@@ -11,20 +11,56 @@ import com.google.android.material.snackbar.Snackbar;
 public class SecondActivity extends AppCompatActivity
 {
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+        findViewById(R.id.btn_execute);
+        Bundle extras =  getIntent().getExtras();
 
-        setTitle("hola");
+      if(extras!=null)
+      {
+          if(extras.getInt("admin") == 1)
+          {
+              setTitle("Admin");
+          }
+         else
+          {
+              setTitle("User");
+          }
+      }
+
+
 
     }
 
+    /**
+     * metode que es cridat en el buto execute
+     * @param vista
+     */
     public void showSnackbar(View vista)
     {
-        Snackbar adminSnackbar =  Snackbar.make(findViewById(R.id.layout), "Benvingut Admin", 1000);
-        adminSnackbar.show();
+
+        Bundle extras =  getIntent().getExtras();
+
+        if(extras!=null)
+        {
+            if(extras.getInt("admin") == 1)
+            {
+
+                Snackbar adminSnaback= Snackbar.make(findViewById(R.id.layout),"Benvingut Admin",1000);
+                adminSnaback.show();
+            }
+            else
+            {
+
+                Snackbar userSnackbar= Snackbar.make(findViewById(R.id.layout),"Benvingut user",1000);
+                userSnackbar.show();
+            }
+        }
+
+
+
     }
 }
