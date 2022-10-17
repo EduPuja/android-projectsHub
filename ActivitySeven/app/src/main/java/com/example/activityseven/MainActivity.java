c4a0ddff2453817846f5832eedcd82057116b087
Eduard <edpufa@inspalamos.cat>
2022-10-17 19:00:50 +0200

    ActivitySeven
    Nova Activitat

---
+++ ActivitySeven/app/src/main/java/com/example/activityseven/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.activityseven;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ListView;
+
+import com.example.activityseven.database.entities.StudentDB;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+}
