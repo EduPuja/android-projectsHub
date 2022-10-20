5384e7d36250a087a29c2cf80578f3e845af8524
Eduard <edpufa@inspalamos.cat>
2022-10-20 17:07:55 +0200

    Update StudientProfileActivity.java

--- ActivitySeven/app/src/main/java/com/example/activityseven/StudientProfileActivity.java
+++ ActivitySeven/app/src/main/java/com/example/activityseven/StudientProfileActivity.java
@@ -2,7 +2,9 @@ package com.example.activityseven;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class StudientProfileActivity extends AppCompatActivity
 {
@@ -15,5 +17,16 @@ public class StudientProfileActivity extends AppCompatActivity
 
         // posar un title amb el nom del alumne i buscar per id cada text view
         // per finalizar fer un set text amb totes les dades
+    }
+
+
+    /**
+     * metode que utliza el boto tornar enrrere
+     * @param vista
+     */
+    public void turnBack(View vista)
+    {
+        Intent goList = new Intent(this,MainActivity.class);
+        startActivity(goList);
     }
 }
