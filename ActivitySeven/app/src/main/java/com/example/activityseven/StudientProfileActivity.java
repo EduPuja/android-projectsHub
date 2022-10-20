159b909dffe6174990a17248f44dfaa420b5059f
Eduard <edpufa@inspalamos.cat>
2022-10-20 16:45:26 +0200

    Refatctor
    StudientProfile -> StudientProfileActivity

--- ActivitySeven/app/src/main/java/com/example/activityseven/StudientProfileActivity.java
+++ ActivitySeven/app/src/main/java/com/example/activityseven/StudientProfileActivity.java
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class StudientProfile extends AppCompatActivity
+public class StudientProfileActivity extends AppCompatActivity
 {
 
     @Override
