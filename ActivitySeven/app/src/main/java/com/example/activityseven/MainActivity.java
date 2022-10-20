20b012abade3e95c67067fd74536a1988fa064e5
Eduard <edpufa@inspalamos.cat>
2022-10-20 14:52:10 +0200

    Update MainActivity.java
    Adding and imports a list view and setContentView Layout

--- ActivitySeven/app/src/main/java/com/example/activityseven/MainActivity.java
+++ ActivitySeven/app/src/main/java/com/example/activityseven/MainActivity.java
@@ -5,11 +5,27 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ListView;
 
+import com.example.activityseven.adapater.StudentAdapter;
 import com.example.activityseven.database.entities.StudentDB;
+import com.example.activityseven.database.model.Student;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity 
 {
 
+    private StudentDB studentDB;
+    private StudentAdapter studentAdapter;
+    private  final ArrayList<Student> listStudens = new ArrayList<>();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ListView listView = findViewById(R.id.list_view);
+        studentDB=new StudentDB(this);
+        studentDB.clearAllItems(); // natejem tots els items
+    }
 }
