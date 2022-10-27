42027a763ceeee49dcaaced12df1f43eda307e11
Eduard <edpufa@inspalamos.cat>
2022-10-27 16:48:33 +0200

    Creating the first Fragment

--- app/src/main/java/edu/pujadas/edu_pujadas_act8/MainActivity.java
+++ app/src/main/java/edu/pujadas/edu_pujadas_act8/MainActivity.java
@@ -2,6 +2,8 @@ package edu.pujadas.edu_pujadas_act8;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 import android.view.View;
@@ -23,6 +25,9 @@ public class MainActivity extends AppCompatActivity
      */
     public void onImgButton(View vista)
     {
-
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.setReorderingAllowed(true);
+        fragmentTransaction.commit();
     }
 }
