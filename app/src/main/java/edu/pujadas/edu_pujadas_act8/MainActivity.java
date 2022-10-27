e5af4019a7cae84ac3fea954c89a6c52a94a4c8e
Eduard <edpufa@inspalamos.cat>
2022-10-27 15:47:17 +0200

    Activity 8

---
+++ app/src/main/java/edu/pujadas/edu_pujadas_act8/MainActivity.java
@@ -0,0 +1,16 @@
+package edu.pujadas.edu_pujadas_act8;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
