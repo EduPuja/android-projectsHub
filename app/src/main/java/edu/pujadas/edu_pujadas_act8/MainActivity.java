fad526c3f7696ec6bf67f90b7b5761be1c3774ff
Eduard <edpufa@inspalamos.cat>
2022-10-27 16:15:28 +0200

    Adding a method

--- app/src/main/java/edu/pujadas/edu_pujadas_act8/MainActivity.java
+++ app/src/main/java/edu/pujadas/edu_pujadas_act8/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity
 {
@@ -14,5 +15,14 @@ public class MainActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         setTitle("Fitxa Producte");
+    }
+
+    /**
+     * metode que crea la imatge
+     * @param vista
+     */
+    public void onImgButton(View vista)
+    {
+
     }
 }
