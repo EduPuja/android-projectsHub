9dd927beb0698fbc4e45ee61a2059c3dc04ce52c
Eduard <edpufa@inspalamos.cat>
2022-11-03 16:02:11 +0100

    ValoracionsAdapter object

--- Eduard_Pujadas_ACT8/app/src/main/java/edu/pujadas/eduard_pujadas_act8/adapters/ValorationsAdapter.java
+++ Eduard_Pujadas_ACT8/app/src/main/java/edu/pujadas/eduard_pujadas_act8/adapters/ValorationsAdapter.java
@@ -1,19 +1,23 @@
 package edu.pujadas.eduard_pujadas_act8.adapters;
 
 import android.content.Context;
+import android.widget.ArrayAdapter;
 
 import java.util.ArrayList;
 
-public class ValorationsAdapter
+import edu.pujadas.eduard_pujadas_act8.Models.Valoracions;
+import edu.pujadas.eduard_pujadas_act8.R;
+
+public class ValorationsAdapter extends ArrayAdapter<Valoracions>
 {
     private final Context context;
-    private final ArrayList<Student> students;
+    private  final ArrayList<Valoracions> listValoracions;
 
-    public ValorationsAdapter(Context context, ArrayList<Student> students)
+    public ValorationsAdapter(Context context, ArrayList<Valoracions> listValoracions)
     {
-        super(context, R.layout.student_list_item);
+        super(context, R.layout.valo_list_item);
         this.context = context;
-        this.students = students;
+        this.listValoracions = listValoracions;
     }
 
 
