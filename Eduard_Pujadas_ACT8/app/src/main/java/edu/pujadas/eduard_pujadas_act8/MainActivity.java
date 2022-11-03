7d24928ee382b179594783eb5a7b2fa41c1053f3
Eduard <edpufa@inspalamos.cat>
2022-11-03 15:03:00 +0100

    Last MainActivity

--- Eduard_Pujadas_ACT8/app/src/main/java/edu/pujadas/eduard_pujadas_act8/MainActivity.java
+++ Eduard_Pujadas_ACT8/app/src/main/java/edu/pujadas/eduard_pujadas_act8/MainActivity.java
@@ -1,8 +1,15 @@
 package edu.pujadas.eduard_pujadas_act8;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.View;
+
+import edu.pujadas.eduard_pujadas_act8.Fragments.FragmentImage;
+
 
 public class MainActivity extends AppCompatActivity
 {
@@ -12,5 +19,32 @@ public class MainActivity extends AppCompatActivity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setTitle("Fitxa Producte");
+
+        // CREACIO DE UN FRAGEMENT
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.setReorderingAllowed(true);
+        fragmentTransaction.add(R.id.container_fragment, new FragmentImage()); // afegiexo !!
+        fragmentTransaction.commit();
+
+    }
+
+    /**
+     * metode que crea la imatge
+     * @param vista
+     */
+    public void onImgButton(View vista)
+    {
+        // CREACIO DE UN FRAGEMENT
+       /* FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.setReorderingAllowed(true);
+
+        // MODIFICIO !!
+        fragmentTransaction.replace(R.id.container_fragment, new FragmentImage());
+        fragmentTransaction.commit();*/
+
+
     }
 }
