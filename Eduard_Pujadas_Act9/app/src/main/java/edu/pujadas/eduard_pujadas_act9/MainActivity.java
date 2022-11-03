6c748795a2f1ba21c445c4e44fac2b55394de7a0
Eduard <edpufa@inspalamos.cat>
2022-11-03 17:06:57 +0100

    Adding clicks methods
    Img and save

--- Eduard_Pujadas_Act9/app/src/main/java/edu/pujadas/eduard_pujadas_act9/MainActivity.java
+++ Eduard_Pujadas_Act9/app/src/main/java/edu/pujadas/eduard_pujadas_act9/MainActivity.java
@@ -3,6 +3,7 @@ package edu.pujadas.eduard_pujadas_act9;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity
 {
@@ -13,5 +14,23 @@ public class MainActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         setTitle("Formulari Producte");
+    }
+
+    /**
+     * Metode que utliza el boto de save
+     * @param vista
+     */
+    public void onSaveButon(View vista)
+    {
+
+    }
+
+    /**
+     * Metode que uilizara el boto de obrir la camara
+     * @param vista
+     */
+    public void onImgButon(View vista)
+    {
+
     }
 }
