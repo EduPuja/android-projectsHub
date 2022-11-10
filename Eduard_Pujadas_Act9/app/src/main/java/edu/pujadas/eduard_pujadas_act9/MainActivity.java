8a047e24272d0b20a8223876e894e805e135eeeb
Eduard <edpufa@inspalamos.cat>
2022-11-10 15:23:37 +0100

    Adiing editText and spinner

--- Eduard_Pujadas_Act9/app/src/main/java/edu/pujadas/eduard_pujadas_act9/MainActivity.java
+++ Eduard_Pujadas_Act9/app/src/main/java/edu/pujadas/eduard_pujadas_act9/MainActivity.java
@@ -4,16 +4,28 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity
 {
-
+    //edit text
+    private EditText marcaProducte;
+    private EditText modelProducte;
+    //spinner
+    private Spinner spinner;
+    // butons
+    private Button saveButon;
+    private Button imgButton;
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         setTitle("Formulari Producte");
+        marcaProducte=findViewById(R.id.marca_product);
+        modelProducte = findViewById(R.id.model_product);
 
 
         //TODO: mirar que el camp no estigui buit
