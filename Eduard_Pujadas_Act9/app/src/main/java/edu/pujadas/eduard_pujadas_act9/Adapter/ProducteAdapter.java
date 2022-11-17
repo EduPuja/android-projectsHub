4fc826d7177a2543c3fe50ed6326771dd944c607
Eduard <edpufa@inspalamos.cat>
2022-11-17 17:17:18 +0100

    Creant el adaptadro de Producte

--- Eduard_Pujadas_Act9/app/src/main/java/edu/pujadas/eduard_pujadas_act9/Adapter/ProducteAdapter.java
+++ Eduard_Pujadas_Act9/app/src/main/java/edu/pujadas/eduard_pujadas_act9/Adapter/ProducteAdapter.java
@@ -1,6 +1,52 @@
 package edu.pujadas.eduard_pujadas_act9.Adapter;
 
-public class ProducteAdapter
-{
+import android.content.Context;
+import android.widget.ArrayAdapter;
 
+import androidx.annotation.NonNull;
+
+import java.util.ArrayList;
+
+import edu.pujadas.eduard_pujadas_act9.Models.Producte;
+import edu.pujadas.eduard_pujadas_act9.R;
+
+public class ProducteAdapter extends ArrayAdapter<Producte>
+{
+    private final Context context;
+    private final ArrayList<Producte> listProductes;
+
+    /**
+     * Constucotr amb el contxes i el arraylist del objecte necessari
+     * @param context
+     * @param listProductes
+     */
+    public ProducteAdapter(Context context, ArrayList<Producte> listProductes)
+    {
+        super(context, R.layout.producte_list_item);
+        this.context = context;
+        this.listProductes= listProductes;
+    }
+
+    @Override
+    public int getCount()
+    {
+        return listProductes.size();
+    }
+
+    @Override
+    public Producte getItem(int position)
+    {
+        return listProductes.get(position);
+    }
+
+    /**
+     * et retorna la posicio del objecte i fas un hasCode per que no hi ha id
+     * @param position
+     * @return
+     */
+    @Override
+    public long getItemId(int position)
+    {
+        return listProductes.get(position).hashCode();
+    }
 }
