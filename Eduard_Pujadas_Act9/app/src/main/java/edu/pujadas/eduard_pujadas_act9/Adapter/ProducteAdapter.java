19f5a17bcd2921329ca15619b59430b1d27833a4
Eduard <edpufa@inspalamos.cat>
2022-11-17 17:22:11 +0100

    SharedPrefernces to adaptador
    Inflo aqui amb les shared preferences

--- Eduard_Pujadas_Act9/app/src/main/java/edu/pujadas/eduard_pujadas_act9/Adapter/ProducteAdapter.java
+++ Eduard_Pujadas_Act9/app/src/main/java/edu/pujadas/eduard_pujadas_act9/Adapter/ProducteAdapter.java
@@ -1,7 +1,15 @@
 package edu.pujadas.eduard_pujadas_act9.Adapter;
 
+import static android.content.Context.MODE_PRIVATE;
+
 import android.content.Context;
+import android.content.SharedPreferences;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 
@@ -48,5 +56,61 @@ public class ProducteAdapter extends ArrayAdapter<Producte>
     public long getItemId(int position)
     {
         return listProductes.get(position).hashCode();
+    }
+
+
+    /**
+     * Metode important necesites una classe anomenada view holeder , esta a sota
+     * @param position
+     * @param convertView
+     * @param parent
+     * @return
+     */
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent)
+    {
+        View view;
+        final ViewHolder viewHolder;
+
+        if (convertView == null || convertView.getTag() == null)
+        {
+            viewHolder = new ViewHolder();
+            view = LayoutInflater.from(context).inflate(R.layout.producte_list_item, parent, false);
+            viewHolder.marca = view.findViewById(R.id.marca);
+            viewHolder.model = view.findViewById(R.id.model);
+            viewHolder.quant = view.findViewById(R.id.quant);
+
+            view.setTag(viewHolder);
+
+        }
+        else
+        {
+            viewHolder = (ViewHolder) convertView.getTag();
+            view = convertView;
+        }
+
+        // shared preferences ??
+        // Set text with the item name
+
+
+
+        SharedPreferences preferences = context.getSharedPreferences("PRODUCTE_DATA", MODE_PRIVATE);
+        String infoMarca = preferences.getString("marca","");
+        String infoModel = preferences.getString("model","");
+        String infoQuant = preferences.getString("quant","");
+        String infoRuta = preferences.getString("img","");
+
+        viewHolder.marca.setText(infoMarca);
+        viewHolder.model.setText(infoModel);
+        viewHolder.quant.setText(infoQuant);
+
+        return view;
+    }
+
+    static class ViewHolder
+    {
+        TextView marca;
+        TextView model;
+        TextView quant;
     }
 }
