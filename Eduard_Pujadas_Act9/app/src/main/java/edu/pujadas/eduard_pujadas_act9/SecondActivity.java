73b583b18b8115099747a045e126e01e2b528de7
Eduard <edpufa@inspalamos.cat>
2022-11-24 15:05:30 +0100

    new Second activity

--- Eduard_Pujadas_Act9/app/src/main/java/edu/pujadas/eduard_pujadas_act9/SecondActivity.java
+++ Eduard_Pujadas_Act9/app/src/main/java/edu/pujadas/eduard_pujadas_act9/SecondActivity.java
@@ -2,33 +2,10 @@ package edu.pujadas.eduard_pujadas_act9;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
-import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.widget.ListView;
-import android.widget.TextView;
-
-import org.w3c.dom.Text;
-
-import java.util.ArrayList;
-
-import edu.pujadas.eduard_pujadas_act9.Adapter.ProducteAdapter;
-import edu.pujadas.eduard_pujadas_act9.Models.Producte;
 
 public class SecondActivity extends AppCompatActivity
 {
-    // texts view que son per fer un set
-    private TextView marca;
-    private TextView model;
-    private TextView quant;
-    //private TextView ruta;
-
-
-    private final ArrayList<Producte> listProductes =new ArrayList<>();
-    private ProducteAdapter producteAdapter;
-    private ListView listView;
-
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -36,34 +13,6 @@ public class SecondActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
 
-        setTitle("Inventari Aula");
-
-        // lsitView
-        listView = findViewById(R.id.producte_list_view);
-
-        //adaptador
-        producteAdapter = new ProducteAdapter(this,listProductes);
-
-
-        //text views que agafo
-
-       // ruta = findViewById(R.id.ruta);
-
-
-        SharedPreferences preferences = getSharedPreferences("PRODUCTE_DATA",MODE_PRIVATE);
-        String infoMarca = preferences.getString("marca","");
-        String infoModel = preferences.getString("model","");
-        String infoQuant = preferences.getString("quant","");
-        String infoRuta = preferences.getString("img","");
-
-
-
-
-        /*marca.setText(infoMarca);
-        model.setText(infoModel);
-        quant.setText(infoQuant);
-       // ruta.setText(infoRuta);*/
-
-        listView.setAdapter(producteAdapter);
+        setTitle("Inventari Producte");
     }
 }
