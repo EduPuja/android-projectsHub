0fcc39d2168d4add1a652354798f06be2e0ec00f
Eduard <edpufa@inspalamos.cat>
2022-11-30 21:19:40 +0100

    macs && tv & all product

--- Eduard_Pujadas_Act9/app/src/main/java/edu/pujadas/eduard_pujadas_act9/ThirdActivity.java
+++ Eduard_Pujadas_Act9/app/src/main/java/edu/pujadas/eduard_pujadas_act9/ThirdActivity.java
@@ -13,22 +13,34 @@ public class ThirdActivity extends AppCompatActivity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_third);
-       int[] product = getIntent().getIntArrayExtra("product");
-       for(int i =0;i<product.length;i++)
-       {
-           if(product[i] ==1)
-           {
-               setTitle("Macs");
-           }
-           else if(product[i] ==2)
-           {
-               setTitle("Pantalles");
-           }
-           else
-           {
-               setTitle("AAA");
-           }
-       }
+
+        // recullo els intents i els hi poso un valor per defecte :D
+        // per poder saber la informacio
+        int mac = getIntent().getIntExtra("product",1);
+        int screen = getIntent().getIntExtra("product",2);
+        int tv = getIntent().getIntExtra("product",3);
+        int keyboard = getIntent().getIntExtra("product",4);
+
+        if(mac == 1)
+        {
+            //ONLY MACS
+            setTitle("MACS");
+        }
+        else if(screen ==2 )
+        {
+            //nomes pantalles
+            setTitle("SCREENS");
+        }
+        else if(tv == 3)
+        {
+            // nomes televisio
+            setTitle("TV");
+        }
+        else if(keyboard ==4)
+        {
+            // nomes teclats
+            setTitle("KEYBOARDS");
+        }
 
 
     }
