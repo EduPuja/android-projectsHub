88fbcc10cf5dafb7de3deb23a82a9a1c23425651
Eduard <edpufa@inspalamos.cat>
2022-12-12 20:06:31 +0100

    Adding the objcet negoci

--- UF2/Activitat1/app/src/main/java/edu/pujadas/activitat1/Models/Negoci.java
+++ UF2/Activitat1/app/src/main/java/edu/pujadas/activitat1/Models/Negoci.java
@@ -2,5 +2,72 @@ package edu.pujadas.activitat1.Models;
 
 public class Negoci
 {
+     int idNegoci;
+     String nom;
+     String tipusNegoci;
+     int latitud;
+     int longitud;
 
+    public Negoci()
+    {
+    }
+
+    public Negoci(int idNegoci, String nom, String tipusNegoci, int latitud, int longitud)
+    {
+        this.idNegoci = idNegoci;
+        this.nom = nom;
+        this.tipusNegoci = tipusNegoci;
+        this.latitud = latitud;
+        this.longitud = longitud;
+    }
+
+    public int getIdNegoci()
+    {
+        return idNegoci;
+    }
+
+    public void setIdNegoci(int idNegoci)
+    {
+        this.idNegoci = idNegoci;
+    }
+
+    public String getNom()
+    {
+        return nom;
+    }
+
+    public void setNom(String nom)
+    {
+        this.nom = nom;
+    }
+
+    public String getTipusNegoci()
+    {
+        return tipusNegoci;
+    }
+
+    public void setTipusNegoci(String tipusNegoci)
+    {
+        this.tipusNegoci = tipusNegoci;
+    }
+
+    public int getLatitud()
+    {
+        return latitud;
+    }
+
+    public void setLatitud(int latitud)
+    {
+        this.latitud = latitud;
+    }
+
+    public int getLongitud()
+    {
+        return longitud;
+    }
+
+    public void setLongitud(int longitud)
+    {
+        this.longitud = longitud;
+    }
 }
