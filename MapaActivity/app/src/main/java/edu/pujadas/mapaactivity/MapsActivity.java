cd188e10814c6464bd87127ae3396246588684a1
Eduard <edpufa@inspalamos.cat>
2022-12-12 19:34:51 +0100

    Tipus mapa

--- MapaActivity/app/src/main/java/edu/pujadas/mapaactivity/MapsActivity.java
+++ MapaActivity/app/src/main/java/edu/pujadas/mapaactivity/MapsActivity.java
@@ -46,7 +46,8 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     public void onMapReady(GoogleMap googleMap)
     {
         mMap = googleMap;
-
+        //tipus mapa
+        mMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
         // Add a marker in Sydney and move the camera
         LatLng sydney = new LatLng(-34, 151);
         mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
