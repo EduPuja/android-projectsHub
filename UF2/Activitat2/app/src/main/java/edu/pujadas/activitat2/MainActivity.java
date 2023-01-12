a56c3b55f7d3f8f11081115191b4b6942ce1b91d
Eduard <edpufa@inspalamos.cat>
2023-01-12 15:59:24 +0100

    backend and onclick

--- UF2/Activitat2/app/src/main/java/edu/pujadas/activitat2/MainActivity.java
+++ UF2/Activitat2/app/src/main/java/edu/pujadas/activitat2/MainActivity.java
@@ -3,14 +3,56 @@ package edu.pujadas.activitat2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.VideoView;
 
 public class MainActivity extends AppCompatActivity
 {
+
+    //imatges
+    ImageView man;
+    ImageView interficia;
+    ImageView reciciclar;
+    ImageView restaurant;
+    ImageView tienda;
+    ImageView viatjar;
+
+    //text views
+    TextView titol;
+    TextView any;
+    TextView descripcio;
+
+    VideoView video;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // imatge view find by id
+        man = findViewById(R.id.man);
+        interficia = findViewById(R.id.interficia);
+        reciciclar = findViewById(R.id.reciclar);
+        restaurant = findViewById(R.id.restaurant);
+        tienda = findViewById(R.id.tienda);
+        viatjar= findViewById(R.id.viatgar);
+
+        // text views
+        titol = findViewById(R.id.titol);
+        any = findViewById(R.id.any);
+        descripcio = findViewById(R.id.descripcio);
+
+        //video
+
+        video = findViewById(R.id.video);
+
+    }
+
+    public void onImgClick(View vista)
+    {
+
     }
 }
