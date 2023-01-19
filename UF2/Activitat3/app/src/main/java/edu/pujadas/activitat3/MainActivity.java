00379451f4a16f184052403a03317c0a43d3e9e9
Eduard <edpufa@inspalamos.cat>
2023-01-19 16:49:08 +0100

    Rest stuff

--- UF2/Activitat3/app/src/main/java/edu/pujadas/activitat3/MainActivity.java
+++ UF2/Activitat3/app/src/main/java/edu/pujadas/activitat3/MainActivity.java
@@ -3,14 +3,27 @@ package edu.pujadas.activitat3;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity
 {
+    EditText usernameFiled;
+    Button btnFollowers;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        usernameFiled.findViewById(R.id.btnFollowers);
+        btnFollowers.findViewById(R.id.btnFollowers);
+    }
+
+    public void onFollowersClick(View vista)
+    {
+
     }
 }
