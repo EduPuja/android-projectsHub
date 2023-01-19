2097680fee75f1898b7087ce69eb7ccb566e4ef4
Eduard <edpufa@inspalamos.cat>
2023-01-19 16:52:30 +0100

    Build Config necessari per fer -ho amb res

---
+++ UF2/Activitat3/app/src/main/java/edu/pujadas/activitat3/rest/BuildConfig.java
@@ -0,0 +1,19 @@
+package edu.pujadas.activitat3.rest;
+
+public final  class BuildConfig
+{
+    public static final boolean DEBUG;
+
+    static
+    {
+        Boolean.parseBoolean("true");
+        DEBUG = true;
+    }
+
+    public static final String APPLICATION_ID = "com.rappadmobile";
+        public static final String BUILD_TYPE = "debug";
+        public static final String FLAVOR = "";
+        public static final int VERSION_CODE = 1;
+        public static final String VERSION_NAME = "1.0";
+
+}
