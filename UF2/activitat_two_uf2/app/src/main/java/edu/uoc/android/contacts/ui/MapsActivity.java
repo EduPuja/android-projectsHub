8cce0f4e60e4977be1015afd0a9825d6a666a794
Eduard <edpufa@inspalamos.cat>
2023-01-19 15:02:04 +0100

    tipus de mapa and zoom

--- UF2/activitat_two_uf2/app/src/main/java/edu/uoc/android/contacts/ui/MapsActivity.java
+++ UF2/activitat_two_uf2/app/src/main/java/edu/uoc/android/contacts/ui/MapsActivity.java
@@ -55,17 +55,21 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
     public void onMapReady(GoogleMap googleMap)
     {
         mMap = googleMap;
-
-        /*// Add a marker in Sydney and move the camera
+        //tipus mapa
+        mMap.setMapType(GoogleMap.MAP_TYPE_TERRAIN);
+        //zoom enambled
+        mMap.getUiSettings().setZoomControlsEnabled(true);
+        // Add a marker in Sydney and move the camera
         LatLng sydney = new LatLng(-34, 151);
         mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));*/
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
     }
 
     public void contacts()
     {
         Contact contact=  new Contact();
         //todo no hi ha setters ni getters ??????
+
 
     }
 }
