18bc5c7ea9f586d490724d0390ff7502cb923af4
Eduard <edpufa@inspalamos.cat>
2023-01-23 19:05:00 +0100

    Activitat FInal

---
+++ UF2/Activitat4_Final/app/src/main/java/edu/pujadas/activitat4_final/MainActivity.java
@@ -0,0 +1,16 @@
+package edu.pujadas.activitat4_final;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
