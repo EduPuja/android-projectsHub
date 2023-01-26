42345d2e98141dd22cb212f9e4e6e2843630c5c9
Eduard <edpufa@inspalamos.cat>
2023-01-26 15:25:16 +0100

    User activity posant un correu

--- UF2/Activitat4_Final/app/src/main/java/edu/pujadas/activitat4_final/UserActivity.java
+++ UF2/Activitat4_Final/app/src/main/java/edu/pujadas/activitat4_final/UserActivity.java
@@ -2,15 +2,38 @@ package edu.pujadas.activitat4_final;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.os.PerformanceHintManager;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+
+import edu.pujadas.activitat4_final.Models.Persona;
 
 public class UserActivity extends AppCompatActivity
 {
+
+    private ArrayList<Persona> listPersona = new ArrayList<Persona>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user);
+
+        //preferenices
+        SharedPreferences sharedPreferences = getSharedPreferences("USER_INFO",MODE_PRIVATE);
+        Gson gson = new Gson();
+        String info = sharedPreferences.getString("listPersona","");
+
+        Type type = new TypeToken<ArrayList<Persona>>() {}.getType();
+        listPersona = gson.fromJson(info,type);
+
+        setTitle(listPersona.get(0).getEmail());
+
     }
 }
