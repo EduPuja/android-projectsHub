d9f9531b5402ad66f137e4bb3ce2c139fe6c6293
Eduard <edpufa@inspalamos.cat>
2023-01-26 18:32:01 +0100

    Update AddHomeActivity.java

--- UF2/Activitat4_Final/app/src/main/java/edu/pujadas/activitat4_final/AddHomeActivity.java
+++ UF2/Activitat4_Final/app/src/main/java/edu/pujadas/activitat4_final/AddHomeActivity.java
@@ -2,11 +2,15 @@ package edu.pujadas.activitat4_final;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
 
 public class AddHomeActivity extends AppCompatActivity
 {
-
+    static final int REQUEST_VIDEO_CAPTURE = 1;
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
@@ -14,5 +18,20 @@ public class AddHomeActivity extends AppCompatActivity
         setContentView(R.layout.activity_add_home);
 
         setTitle("Add Home");
+    }
+
+    public void openCamera(View vista)
+    {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        try
+        {
+            startActivityForResult(takePictureIntent, REQUEST_VIDEO_CAPTURE);
+        }
+        catch (ActivityNotFoundException e)
+        {
+                // display error state to the user
+            e.printStackTrace();
+        }
+
     }
 }
