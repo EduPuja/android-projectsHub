a1b5da02fe8e0acd14e71a7001d775d4762141d4
Eduard <edpufa@inspalamos.cat>
2023-01-26 18:19:25 +0100

    AddHomeActivity

---
+++ UF2/Activitat4_Final/app/src/main/java/edu/pujadas/activitat4_final/AddHomeActivity.java
@@ -0,0 +1,18 @@
+package edu.pujadas.activitat4_final;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AddHomeActivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_home);
+
+        setTitle("Add Home");
+    }
+}
