d3005e99a520886b67fe6f7b8206d4d9822019e9
Eduard <edpufa@inspalamos.cat>
2023-01-26 15:20:04 +0100

    Creating userActivity

---
+++ UF2/Activitat4_Final/app/src/main/java/edu/pujadas/activitat4_final/UserActivity.java
@@ -0,0 +1,16 @@
+package edu.pujadas.activitat4_final;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class UserActivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user);
+    }
+}
