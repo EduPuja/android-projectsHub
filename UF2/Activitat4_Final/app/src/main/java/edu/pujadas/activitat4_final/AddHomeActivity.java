4430a991396dc517cdd1a24b1af36bac6b2910cf
Eduard <edpufa@inspalamos.cat>
2023-01-26 19:16:01 +0100

    Adding the method insertOne
    Insert a new home

--- UF2/Activitat4_Final/app/src/main/java/edu/pujadas/activitat4_final/AddHomeActivity.java
+++ UF2/Activitat4_Final/app/src/main/java/edu/pujadas/activitat4_final/AddHomeActivity.java
@@ -4,19 +4,39 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.ActivityNotFoundException;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.io.Serializable;
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+
+import edu.pujadas.activitat4_final.Models.Home;
 
 public class AddHomeActivity extends AppCompatActivity
 {
     static final int REQUEST_VIDEO_CAPTURE = 1;
+    EditText nameCity;
+    Button openCamera;
+    Button insertHome;
+    ArrayList<Home> listHomes = new ArrayList<Home>();
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_home);
-
+        nameCity = findViewById(R.id.nameCity);
+        openCamera = findViewById(R.id.openCamera);
+        insertHome = findViewById(R.id.insertHome);
         setTitle("Add Home");
     }
 
@@ -26,11 +46,35 @@ public class AddHomeActivity extends AppCompatActivity
         try
         {
             startActivityForResult(takePictureIntent, REQUEST_VIDEO_CAPTURE);
+
         }
         catch (ActivityNotFoundException e)
         {
                 // display error state to the user
             e.printStackTrace();
+        }
+
+    }
+
+    public void insertOne(View vista)
+    {
+        if(!nameCity.getText().toString().isEmpty())
+        {
+            Home home= new Home(nameCity.getText().toString(), BitmapFactory.decodeResource(getBaseContext().getResources(),R.drawable.casaa));
+
+            SharedPreferences homePreferences = getSharedPreferences("HOME_INFO",MODE_PRIVATE);
+            Gson gson = new Gson();
+            String info = homePreferences.getString("listHomes","");
+            Type type = new TypeToken<ArrayList<Home>>() {}.getType();
+            listHomes= gson.fromJson(info,type);
+
+            listHomes.add(home);
+            System.out.println("Home inserted");
+
+        }
+        else
+        {
+            nameCity.setError("Name Empty");
         }
 
     }
