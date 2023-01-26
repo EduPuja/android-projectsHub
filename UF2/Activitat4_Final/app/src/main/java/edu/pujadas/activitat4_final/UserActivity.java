6bd48c2ffd29199d25eb82c2bfa3e6d0d3ed1b79
Eduard <edpufa@inspalamos.cat>
2023-01-26 16:57:15 +0100

    Creating 3 methods
    1. shared preferences
    2. init data to home arralist
    3. init recicleView - adds recicleview and home adapter

--- UF2/Activitat4_Final/app/src/main/java/edu/pujadas/activitat4_final/UserActivity.java
+++ UF2/Activitat4_Final/app/src/main/java/edu/pujadas/activitat4_final/UserActivity.java
@@ -1,8 +1,11 @@
 package edu.pujadas.activitat4_final;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.SharedPreferences;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.os.PerformanceHintManager;
 
@@ -12,19 +15,41 @@ import com.google.gson.reflect.TypeToken;
 import java.lang.reflect.Type;
 import java.util.ArrayList;
 
+import edu.pujadas.activitat4_final.Models.Home;
+import edu.pujadas.activitat4_final.Models.HomeAdapter;
 import edu.pujadas.activitat4_final.Models.Persona;
 
 public class UserActivity extends AppCompatActivity
 {
+    // arraylist de persones per poder afegir el titol
+     private ArrayList<Persona> listPersona = new ArrayList<Persona>();
+     //arryalist necessari per poder afegir les cases
+     private ArrayList<Home> listHomes = new ArrayList<Home>();
 
-    private ArrayList<Persona> listPersona = new ArrayList<Persona>();
-
+     RecyclerView recyclerView ;
+     HomeAdapter homeAdapter;
+    LinearLayoutManager layoutManager;
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user);
 
+        // metode shared prefenrece
+        preferencies();
+
+        // metode que inicia dades falses al arraylist de cases
+        initData();
+
+        // metode que crea el reciclerview
+        initRecylcerView();
+    }
+
+    /**
+     * Metode que utliza el shared preferences
+     */
+    private void preferencies()
+    {
         //preferenices
         SharedPreferences sharedPreferences = getSharedPreferences("USER_INFO",MODE_PRIVATE);
         Gson gson = new Gson();
@@ -35,6 +60,34 @@ public class UserActivity extends AppCompatActivity
         // per mostrar el ultim  persona registrada
         int ultim = listPersona.size()-1;
         setTitle("Hello " + listPersona.get(ultim).getEmail());
+    }
 
+    /**
+     * Metode que afegiex dades en el recicleView
+     */
+    private void initData()
+    {
+        //creant un objecte casa
+        Home homePals= new Home("Pals",BitmapFactory.decodeResource(getBaseContext().getResources(),R.drawable.casaa));
+        Home homePalamos= new Home("Palamos",BitmapFactory.decodeResource(getBaseContext().getResources(),R.drawable.casab));
+        Home homeCasaC= new Home("Vallobrega",BitmapFactory.decodeResource(getBaseContext().getResources(),R.drawable.casac));
+        Home homeCasaD= new Home("Mataró",BitmapFactory.decodeResource(getBaseContext().getResources(),R.drawable.casad));
+
+        listHomes.add(homePals);
+        listHomes.add(homePalamos);
+        listHomes.add(homeCasaC);
+        listHomes.add(homeCasaD);
+    }
+
+    private void initRecylcerView()
+    {
+        //recicleview
+        recyclerView = findViewById(R.id.recicleView);
+        layoutManager = new LinearLayoutManager(this);
+        layoutManager.setOrientation(RecyclerView.VERTICAL);
+        recyclerView.setLayoutManager(layoutManager);
+        homeAdapter = new HomeAdapter(listHomes);
+        recyclerView.setAdapter(homeAdapter);
+        homeAdapter.notifyDataSetChanged();
     }
 }
