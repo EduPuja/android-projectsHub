748c09b1c2949de6e0fcd14011f66e1e3e8f81d4
Eduard <edpufa@inspalamos.cat>
2023-01-31 00:36:53 +0100

    new id

---
+++ UF2/Activitat3/app/src/main/java/edu/pujadas/activitat3/FollowersActivity.java
@@ -0,0 +1,16 @@
+package edu.pujadas.activitat3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FollowersActivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_followers);
+    }
+}
