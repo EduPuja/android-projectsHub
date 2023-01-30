c040afee738727d72296825467d225ad95c05ff3
Eduard <edpufa@inspalamos.cat>
2023-01-31 00:37:05 +0100

    Fixing the bug

--- UF2/Activitat3/app/src/main/java/edu/pujadas/activitat3/MainActivity.java
+++ UF2/Activitat3/app/src/main/java/edu/pujadas/activitat3/MainActivity.java
@@ -2,6 +2,7 @@ package edu.pujadas.activitat3;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -18,12 +19,12 @@ public class MainActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        usernameFiled.findViewById(R.id.btnFollowers);
-        btnFollowers.findViewById(R.id.btnFollowers);
+        usernameFiled =findViewById(R.id.usernameFiled);
+        btnFollowers = findViewById(R.id.btnFollowers);
     }
 
     public void onFollowersClick(View vista)
     {
-
+        Intent intent = new Intent(MainActivity.this, FollowersActivity.class);
     }
 }
