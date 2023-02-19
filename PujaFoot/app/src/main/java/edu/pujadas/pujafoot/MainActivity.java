d64f70c19a20026b959fd1965e90e4b5769a80ca
EduPuja <edpufa@inspalamos.cat>
2023-02-19 23:08:44 +0100

    OnCick i validate data

--- PujaFoot/app/src/main/java/edu/pujadas/pujafoot/MainActivity.java
+++ PujaFoot/app/src/main/java/edu/pujadas/pujafoot/MainActivity.java
@@ -2,10 +2,20 @@ package edu.pujadas.pujafoot;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity
 {
+
+    EditText userName;
+    EditText password;
+
+    Button submitBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -13,7 +23,44 @@ public class MainActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         setTitle("PujaFoot");
+        userName = findViewById(R.id.username);
+        password = findViewById(R.id.password);
+        submitBtn = findViewById(R.id.submitBtn);
 
 
+
+
+    }
+
+    public void onClick(View v)
+    {
+        if(validateData())
+        {
+            Intent  intent = new Intent(this, UserActivity.class);
+            startActivity(intent);
+            finish();
+        }
+
+
+    }
+
+    public boolean validateData()
+    {
+        if(userName.getText().toString().isEmpty())
+        {
+            userName.setError("Introudeix el usuari");
+
+        }
+        else if(password.getText().toString().isEmpty())
+        {
+            password.setError("Introudeix el password");
+        }
+        else
+        {
+            if(userName.getText().toString().equalsIgnoreCase("epujadas") && password.getText().toString().equalsIgnoreCase("123asd123"))
+                return true;
+
+        }
+        return false;
     }
 }
