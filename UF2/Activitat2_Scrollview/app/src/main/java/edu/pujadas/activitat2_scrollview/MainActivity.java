e33fc4be97599928471c2206c7970286df0f00f8
Edu <edpufa@inspalamos.cat>
2023-12-14 19:57:29 +0100

    Activitat 2 2023

---
+++ UF2/Activitat2_Scrollview/app/src/main/java/edu/pujadas/activitat2_scrollview/MainActivity.java
@@ -0,0 +1,102 @@
+package edu.pujadas.activitat2_scrollview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.HorizontalScrollView;
+import android.widget.ImageView;
+import android.widget.ScrollView;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.widget.VideoView;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    TextView title,year,description;
+    HorizontalScrollView scrollView;
+
+    ImageView car_1, car_2, car_3, car_4, car_5, car_6;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //find by id
+        title = findViewById(R.id.title);
+        year = findViewById(R.id.year);
+        description = findViewById(R.id.descrip);
+        title.setText("");
+        year.setText("");
+        description.setText("");
+
+        scrollView = findViewById(R.id.scroll_view);
+        // find the images by the id
+        car_1 = findViewById(R.id.car1);
+        car_2 = findViewById(R.id.car2);
+        car_3 = findViewById(R.id.car3);
+        car_4 = findViewById(R.id.car4);
+        car_5 = findViewById(R.id.car5);
+        car_6 = findViewById(R.id.car6);
+
+
+        car_1.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                //Toast.makeText(view.getContext(), "hola", Toast.LENGTH_LONG).show();
+                setText("Volwavgent","1999","Mejor cotche de los años 90");
+            }
+        });
+
+        car_2.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                //Toast.makeText(view.getContext(), "hola", Toast.LENGTH_LONG).show();
+                setText("Toyota Supra","2000","Clascio de las peliculas");
+            }
+        });
+        car_3.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                //Toast.makeText(view.getContext(), "hola", Toast.LENGTH_LONG).show();
+                setText("Tesla","2020","Una lavadora, no sirve de mucho ");
+            }
+        });
+        car_4.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                //Toast.makeText(view.getContext(), "hola", Toast.LENGTH_LONG).show();
+                setText("Subauwu","1992","El mejor 1r coche de drift del mundo");
+            }
+        });
+        car_5.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                setText("Mitshubishi Evo","2000","El segundo mejor coche de drift del la historia");
+            }
+        });
+        car_6.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                //Toast.makeText(view.getContext(), "hola", Toast.LENGTH_LONG).show();
+                setText("Range Rover","2008", "Coche de ricos ");
+            }
+        });
+
+    }
+
+    private void setText(String titol,String any,String descrip) {
+        title.setText("Titol: "+titol);
+        year.setText("Any : "+any);
+        description.setText("Descrip :"+descrip);
+
+
+    }
+}
