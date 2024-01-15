50bca14ded97e74a132a579a7cd81618e0d4b99b
Edu <eduard.pujadas@gmail.com>
2024-01-15 19:36:01 +0100

    Fent activitat

--- UF2/Activitat3-Mapes/app/src/main/java/edu/pujadas/MainActivity.java
+++ UF2/Activitat3-Mapes/app/src/main/java/edu/pujadas/MainActivity.java
@@ -3,12 +3,35 @@ package edu.pujadas;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+
+import com.google.android.gms.maps.MapView;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    MapView mapView;
+    Button restaurantBtn, peluqueriaBtn,mecanicBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        //find by id
+        mapView = findViewById(R.id.mapView);
+        restaurantBtn = findViewById(R.id.restaurantBtn);
+        peluqueriaBtn = findViewById(R.id.peluqueriaBtn);
+        mecanicBtn = findViewById(R.id.mecanicBtn);
+
+
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mapView.onResume();
     }
 }
