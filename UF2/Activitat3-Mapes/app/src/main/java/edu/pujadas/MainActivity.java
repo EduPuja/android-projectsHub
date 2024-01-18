902f21e82b6b75cacdcecb79c652a0d7c9ad547f
Edu <eduard.pujadas@gmail.com>
2024-01-18 17:29:36 +0100

    afegint el hasmap

--- UF2/Activitat3-Mapes/app/src/main/java/edu/pujadas/MainActivity.java
+++ UF2/Activitat3-Mapes/app/src/main/java/edu/pujadas/MainActivity.java
@@ -7,18 +7,22 @@ import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapView;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.maps.model.LatLng;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 import edu.pujadas.Models.Tienda;
 import edu.pujadas.Models.Tipus;
@@ -29,11 +33,15 @@ public class MainActivity extends AppCompatActivity {
     MapView mapView ;
     Button restaurantBtn, peluqueriaBtn,mecanicBtn;
     ArrayList<Tienda> listTiendas;
+    HashMap<String, Marker> markerHashMap ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // !hasmap the markers
+
+        markerHashMap = new HashMap<String, Marker>();
 
         //!find by id
         restaurantBtn = findViewById(R.id.restaurantBtn);
@@ -49,7 +57,7 @@ public class MainActivity extends AppCompatActivity {
         Tienda tienda = new Tienda(1,"Can Valdiri", Tipus.RESTAURANT, new LatLng(41.303169,2.003210f));
         Tienda tienda2 = new Tienda(2,"Motos Ferrer", Tipus.MECANIC,new LatLng(41.385063, 2.173404f));
         Tienda tienda3 = new Tienda(3,"CCCC", Tipus.RESTAURANT,new LatLng(39.569450,2.649950f));
-        Tienda tienda4 = new Tienda(4,"CCCC", Tipus.PELUQUERIA,new LatLng(40.416775,3.703790f));
+        Tienda tienda4 = new Tienda(4,"CCCC", Tipus.PELUQUERIA,new LatLng(40.416775,-3.703790f));
 
         listTiendas = new ArrayList<Tienda>();
         //!adding the tiendas
@@ -75,10 +83,16 @@ public class MainActivity extends AppCompatActivity {
                         // resutanrat amb el tamany que te la foto
                         Bitmap restaurantIcon = BitmapFactory.decodeResource(getResources(), R.drawable.restaurnat);
                         //restuarnat petit
-                        Bitmap tinnyRestaurant = Bitmap.createScaledBitmap(restaurantIcon,alutra,ancho,true);
+                        Bitmap tinnyRestaurant = Bitmap.createScaledBitmap(restaurantIcon,alutra,ancho,false);
 
-                        googleMap.addMarker(new MarkerOptions().position(posi).title("Restaurant").icon(BitmapDescriptorFactory.fromBitmap(tinnyRestaurant)));
+                        Marker marker = googleMap.addMarker(new MarkerOptions()
+                                .position(posi)
+                                .title("Restaurant")
+                                .icon(BitmapDescriptorFactory.fromBitmap(tinnyRestaurant)));
+                        googleMap.moveCamera(CameraUpdateFactory.newLatLng(posi));
 
+                        //! add the marker to hasmap
+                        markerHashMap.put("restaurant_marker",marker);
 
                     }
                     else if (t.getType() == Tipus.MECANIC)
@@ -86,18 +100,29 @@ public class MainActivity extends AppCompatActivity {
                         Bitmap mecanic = BitmapFactory.decodeResource(getResources(), R.drawable.mecanico);
 
                         // faig aquest cambi a pettit perque el bitmap de adalt es inmuttable
-                        Bitmap tinnyMecanic = Bitmap.createScaledBitmap(mecanic,alutra,ancho,true);
+                        Bitmap tinnyMecanic = Bitmap.createScaledBitmap(mecanic,alutra,ancho,false);
 
-                        googleMap.addMarker(new MarkerOptions().position(posi).title("Mecanic").icon(BitmapDescriptorFactory.fromBitmap(tinnyMecanic)));
+                        Marker marker =googleMap.addMarker(new MarkerOptions()
+                                .position(posi)
+                                .title("Mecanic")
+                                .icon(BitmapDescriptorFactory.fromBitmap(tinnyMecanic)));
+
+
+                        //add the marker to hasmap
+                        markerHashMap.put("mecanic_marker",marker);
+
 
                     }
                     else if (t.getType() == Tipus.PELUQUERIA){
                         Bitmap pelu = BitmapFactory.decodeResource(getResources(), R.drawable.pelu);
 
                         //mini pelu
-                        Bitmap peluTinny = Bitmap.createScaledBitmap(pelu,alutra,ancho,true);
+                        Bitmap peluTinny = Bitmap.createScaledBitmap(pelu,alutra,ancho,false);
 
-                        googleMap.addMarker(new MarkerOptions().position(posi).title("Peluqueria").icon(BitmapDescriptorFactory.fromBitmap(peluTinny)));
+                        Marker marker = googleMap.addMarker(new MarkerOptions().position(posi).title("Peluqueria").icon(BitmapDescriptorFactory.fromBitmap(peluTinny)));
+
+                        //add to the hashmap
+                        markerHashMap.put("peluqueria_marker",marker);
                     }
 
 
@@ -106,6 +131,24 @@ public class MainActivity extends AppCompatActivity {
             }
 
 
+        });
+
+
+
+        //!buttons
+
+        restaurantBtn.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                for (Tienda t : listTiendas)
+                {
+                    if (t.getType() != Tipus.RESTAURANT)
+                    {
+
+                    }
+                }
+            }
         });
 
 
