64c492cd2cf2ca4b32ee5f5002bb94e691e42c81
Edu <eduard.pujadas@gmail.com>
2024-01-22 19:10:22 +0100

    Add a new Activity
    Owner activit was added

---
+++ UF2/activity_three_uf2/app/src/main/java/edu/uoc/android/restservice/ui/enter/OwnerActivity.java
@@ -0,0 +1,16 @@
+package edu.uoc.android.restservice.ui.enter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import edu.uoc.android.restservice.R;
+
+public class OwnerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_owner);
+    }
+}
