3c45b384c2ce123acf8833635a214f6a875fb5ad
Edu <eduard.pujadas@gmail.com>
2024-01-22 19:53:51 +0100

    Posant un menu per tornar

--- UF2/activity_three_uf2/app/src/main/java/edu/uoc/android/restservice/ui/enter/OwnerActivity.java
+++ UF2/activity_three_uf2/app/src/main/java/edu/uoc/android/restservice/ui/enter/OwnerActivity.java
@@ -2,7 +2,11 @@ package edu.uoc.android.restservice.ui.enter;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -25,4 +29,30 @@ public class OwnerActivity extends AppCompatActivity {
         followers  = findViewById(R.id.followers);
 
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.back:
+
+                Intent intent = new Intent(this, EnterUserActivity.class);
+                startActivity(intent);
+                return true;
+
+                default:
+                    return super.onOptionsItemSelected(item);
+
+
+        }
+
+        
+    }
+
 }
