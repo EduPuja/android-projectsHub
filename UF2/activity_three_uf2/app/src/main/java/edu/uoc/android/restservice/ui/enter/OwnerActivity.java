ba8c9db78aec7089ec2b9dd10b35b2a38235126a
Edu <eduard.pujadas@gmail.com>
2024-01-22 19:28:58 +0100

    find by id

--- UF2/activity_three_uf2/app/src/main/java/edu/uoc/android/restservice/ui/enter/OwnerActivity.java
+++ UF2/activity_three_uf2/app/src/main/java/edu/uoc/android/restservice/ui/enter/OwnerActivity.java
@@ -3,14 +3,26 @@ package edu.uoc.android.restservice.ui.enter;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import edu.uoc.android.restservice.R;
 
 public class OwnerActivity extends AppCompatActivity {
 
+    ImageView avatar;
+    TextView repos,followers;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_owner);
+
+
+        //!Find by id
+        avatar = findViewById(R.id.avatar);
+        repos= findViewById(R.id.repos);
+        followers  = findViewById(R.id.followers);
+
     }
 }
