0e48e40a82cf1ed1b43b86ca1740a51500358b97
Edu <eduard.pujadas@gmail.com>
2024-01-25 16:59:33 +0100

    creant el adapter
    todo cambiar les imatges

---
+++ UF2/RecyclerViewEmpty/app/src/main/java/edu/pujadas/adapter/HomeAdapater.java
@@ -0,0 +1,80 @@
+package edu.pujadas.adapter;
+
+import android.graphics.Bitmap;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import edu.pujadas.R;
+import edu.pujadas.models.Home;
+
+public class HomeAdapter extends RecyclerView.Adapter<HomeAdapter.ViewHolder>
+{
+    private List<Home> listHomes = new ArrayList<Home>();
+
+    public HomeAdapter(List<Home> listHomes)
+    {
+        this.listHomes=listHomes;
+    }
+
+    @NonNull
+    @Override
+    public HomeAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType)
+    {  // Create a new view, which defines the UI of the list item
+        View vista = LayoutInflater.from(parent.getContext()).inflate(R.layout.home_item,parent,false);
+
+
+        return new ViewHolder(vista) ;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull HomeAdapter.ViewHolder holder, int position)
+    {
+        // items necessaris per poder afegir els objectes
+        String nomHome = listHomes.get(position).getName();
+        Bitmap img = listHomes.get(position).getImg();
+
+        holder.setData(nomHome,img);
+    }
+
+    @Override
+    public int getItemCount()
+    {
+        return listHomes.size();
+    }
+
+
+    /**
+     * Classe de viewHolder necessari per poder-lo fer amb reicle vie
+     */
+    public static class ViewHolder extends RecyclerView.ViewHolder
+    {
+        ImageView homeImg;
+        TextView textHome;
+        public ViewHolder(@NonNull View itemView)
+        {
+            super(itemView);
+            homeImg = itemView.findViewById(R.id.homeImg);
+            textHome = itemView.findViewById(R.id.textHome);
+        }
+
+        /**
+         * Metode que afegeix tots els caps del recicleview del objecte que tu tinguis
+         * @param nomHome String nom casa
+         * @param img imatge
+         */
+        public void setData(String nomHome, Bitmap img)
+        {
+            homeImg.setImageBitmap(img);
+            textHome.setText(nomHome);
+        }
+    }
+}
