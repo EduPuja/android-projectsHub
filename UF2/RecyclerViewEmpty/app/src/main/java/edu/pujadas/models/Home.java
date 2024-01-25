6fd337adffae2915d9960e5ffb60141f490eaf35
Edu <eduard.pujadas@gmail.com>
2024-01-25 16:59:23 +0100

    cerant el objecte casa

---
+++ UF2/RecyclerViewEmpty/app/src/main/java/edu/pujadas/models/Home.java
@@ -0,0 +1,40 @@
+package edu.pujadas.models;
+
+import android.graphics.Bitmap;
+
+public class Home
+{
+    private String name;
+    private Bitmap img;
+
+    public Home()
+    {
+
+    }
+
+    public Home(String name, Bitmap img)
+    {
+        this.name = name;
+        this.img = img;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+
+    public Bitmap getImg()
+    {
+        return img;
+    }
+
+    public void setImg(Bitmap img)
+    {
+        this.img = img;
+    }
+}
