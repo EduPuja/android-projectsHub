330d0488d121675cb8f64c22b92165d237ed1c1e
Edu <eduard.pujadas@gmail.com>
2024-01-25 17:09:33 +0100

    el padding not working

--- UF2/RecyclerViewEmpty/app/src/main/java/edu/pujadas/MainActivity.java
+++ UF2/RecyclerViewEmpty/app/src/main/java/edu/pujadas/MainActivity.java
@@ -23,6 +23,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        listHomes = new ArrayList<>();
         if(listHomes.isEmpty())
         {
             initData();
