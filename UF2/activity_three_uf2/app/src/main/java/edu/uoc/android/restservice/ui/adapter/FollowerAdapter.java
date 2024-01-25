c0c23bd5c78afb47df8e89bbf82ffb640189f88b
Edu <eduard.pujadas@gmail.com>
2024-01-25 18:24:11 +0100

    Fixing the errors

--- UF2/activity_three_uf2/app/src/main/java/edu/uoc/android/restservice/ui/adapter/FollowerAdapter.java
+++ UF2/activity_three_uf2/app/src/main/java/edu/uoc/android/restservice/ui/adapter/FollowerAdapter.java
@@ -1,39 +1,7 @@
 package edu.uoc.android.restservice.ui.adapter;
-
-import android.view.ViewGroup;
-
-import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import edu.uoc.android.restservice.rest.model.Follower;
-
-public class FollowerAdapter extends RecyclerView.Adapter<Follower>{
-
-     List<Follower> listFollowers = new ArrayList<Follower>();
-
-    public FollowerAdapter(List<Follower> listFollowers) {
-        this.listFollowers = listFollowers;
-    }
-
-    @NonNull
-    @Override
-    public Follower onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        //todo do de adapter
+public class FollowerAdapter extends RecyclerView.Adapter<FollowerAdapter.ViewHolder>{
 
 
-
-    }
-
-    @Override
-    public void onBindViewHolder(@NonNull Follower holder, int position) {
-
-    }
-
-    @Override
-    public int getItemCount() {
-        return 0;
-    }
 }
--- UF2/activity_three_uf2/app/src/main/java/edu/uoc/android/restservice/ui/enter/EnterUserActivity.java
+++ UF2/activity_three_uf2/app/src/main/java/edu/uoc/android/restservice/ui/enter/EnterUserActivity.java
@@ -9,10 +9,11 @@ import android.widget.EditText;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-import edu.uoc.android.restservice.R;
-import edu.uoc.android.restservice.rest.model.Owner;
 
-public class EnterUserActivity extends AppCompatActivity implements View.OnClickListener {
+import edu.uoc.android.restservice.R;
+
+
+public class EnterUserActivity extends AppCompatActivity {
 
     private EditText etUser;
     private Button btnFollowers;
@@ -27,24 +28,25 @@ public class EnterUserActivity extends AppCompatActivity implements View.OnClick
     private void initViews() {
         etUser = findViewById(R.id.enter_user_edit_text);
         btnFollowers = findViewById(R.id.enter_user_button);
-        btnFollowers.setOnClickListener(this);
-    }
 
-    @Override
-    public void onClick(View v) {
-        if (v == btnFollowers) {
-            String username = etUser.getText().toString();
-            if (username.isEmpty())
-            {
-                etUser.setError("Please enter a username");
-            }else
-            {
-                Intent intent = new Intent(v.getContext(), OwnerActivity.class);
-                intent.putExtra("username",username);
-                startActivity(intent);
+        btnFollowers.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                if (v == btnFollowers) {
+                    String username = etUser.getText().toString();
+                    if (username.isEmpty()) {
+                        etUser.setError("Please enter a username");
+                    }
+                    else
+                    {
+                        Intent intent = new Intent(v.getContext(), OwnerActivity.class);
+                        intent.putExtra("username",username);
+                        startActivity(intent);
+                    }
+                }
             }
+        });
 
-
-        }
     }
 }
