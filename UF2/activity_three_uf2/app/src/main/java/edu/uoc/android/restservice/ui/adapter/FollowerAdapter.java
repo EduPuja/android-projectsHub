96c885c6016db50bbe246fab50722be0712ba21f
Edu <eduard.pujadas@gmail.com>
2024-01-25 18:31:29 +0100

    Creant el adaptador

--- UF2/activity_three_uf2/app/src/main/java/edu/uoc/android/restservice/ui/adapter/FollowerAdapter.java
+++ UF2/activity_three_uf2/app/src/main/java/edu/uoc/android/restservice/ui/adapter/FollowerAdapter.java
@@ -1,7 +1,30 @@
 package edu.uoc.android.restservice.ui.adapter;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 public class FollowerAdapter extends RecyclerView.Adapter<FollowerAdapter.ViewHolder>{
 
 
+    @NonNull
+    @Override
+    public FollowerAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull FollowerAdapter.ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public static class ViewHolder {
+
+
+    }
 }
