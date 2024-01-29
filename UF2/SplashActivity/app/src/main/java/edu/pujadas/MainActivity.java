0d9ed8e942565f23ada05e15f286f85d56593a35
Edu <eduard.pujadas@gmail.com>
2024-01-29 22:09:00 +0100

    fent coses

--- UF2/SplashActivity/app/src/main/java/edu/pujadas/MainActivity.java
+++ UF2/SplashActivity/app/src/main/java/edu/pujadas/MainActivity.java
@@ -3,13 +3,28 @@ package edu.pujadas;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    Button okButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         setTitle("hola mon");
+
+
+        okButton = findViewById(R.id.okButton);
+
+        okButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
