bd89b532e016b29dfdadd0d86c253d5191b734b1
Edu <eduard.pujadas@gmail.com>
2024-02-01 19:06:55 +0100

    passant directament al main

--- UF2/SplashActivity/app/src/main/java/edu/pujadas/LoginActivity.java
+++ UF2/SplashActivity/app/src/main/java/edu/pujadas/LoginActivity.java
@@ -2,6 +2,7 @@ package edu.pujadas;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
@@ -20,34 +21,44 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        SharedPreferences sharedPreferences = getSharedPreferences("info", MODE_PRIVATE);
+        String emialPref = sharedPreferences.getString("email","");
+        if (!emialPref.isEmpty())
+        {
+            Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+            startActivity(intent);
+            finish();
+        }
+        else {
+            //find by id
+            email = findViewById(R.id.email);
+            password =  findViewById(R.id.password);
+            okButton = findViewById(R.id.okButton);
 
 
-        //find by id
-        email = findViewById(R.id.email);
-        password =  findViewById(R.id.password);
-        okButton = findViewById(R.id.okButton);
+            //lisener onlcick
+            okButton.setOnClickListener(new View.OnClickListener() {
 
+                @Override
+                public void onClick(View v) {
+                    if (email.getText().toString().isEmpty() || password.getText().toString().isEmpty())
+                    {
+                        email.setError("Camp Vuit");
+                        password.setError("Camp Vuit");
+                    }
+                    else {
+                        String emailStr = email.getText().toString();
 
-        //lisener onlcick
-        okButton.setOnClickListener(new View.OnClickListener() {
+                        SharedPreferences.Editor editor = sharedPreferences.edit();
+                        editor.putString("email",emailStr);
 
-            @Override
-            public void onClick(View v) {
-                if (email.getText().toString().isEmpty() || password.getText().toString().isEmpty())
-                {
-                    email.setError("Camp Vuit");
-                    password.setError("Camp Vuit");
+                        editor.apply();// save changes
+
+                    }
                 }
-                else {
-                    String emailStr = email.getText().toString();
-                    SharedPreferences sharedPreferences = getSharedPreferences("info", MODE_PRIVATE);
-                    SharedPreferences.Editor editor = sharedPreferences.edit();
-                    editor.putString("email",emailStr);
+            });
+        }
 
-                    editor.apply();// save changes
 
-                }
-            }
-        });
     }
 }
