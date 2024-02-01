5063206ca5ae7ef579ba58c371ab777669e1b511
Edu <eduard.pujadas@gmail.com>
2024-02-01 18:44:14 +0100

    posant el titol del email

--- UF2/SplashActivity/app/src/main/java/edu/pujadas/MainActivity.java
+++ UF2/SplashActivity/app/src/main/java/edu/pujadas/MainActivity.java
@@ -2,6 +2,7 @@ package edu.pujadas;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,5 +11,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        SharedPreferences pref = getSharedPreferences("info",MODE_PRIVATE);
+        String email = pref.getString("email","");
+
+        setTitle(email);
     }
 }
