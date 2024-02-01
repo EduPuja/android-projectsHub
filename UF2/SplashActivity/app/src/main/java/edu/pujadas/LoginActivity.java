f1cd609fc16f90566c01623b7cbababcec793087
Edu <eduard.pujadas@gmail.com>
2024-02-01 18:38:45 +0100

    creant una nova acitivty on es mostrara el shared prefernces

--- UF2/SplashActivity/app/src/main/java/edu/pujadas/LoginActivity.java
+++ UF2/SplashActivity/app/src/main/java/edu/pujadas/LoginActivity.java
@@ -2,30 +2,51 @@ package edu.pujadas;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity {
 
 
 
-    //todo fer la splash activity
+
     Button okButton;
+    EditText email,password;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
 
-
+        //find by id
+        email = findViewById(R.id.email);
+        password =  findViewById(R.id.password);
         okButton = findViewById(R.id.okButton);
 
+
+        //lisener onlcick
         okButton.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View v) {
+                if (email.getText().toString().isEmpty() || password.getText().toString().isEmpty())
+                {
+                    email.setError("Camp Vuit");
+                    password.setError("Camp Vuit");
+                }
+                else {
+                    String emailStr = email.getText().toString();
+                    SharedPreferences sharedPreferences = getSharedPreferences("info", MODE_PRIVATE);
+                    SharedPreferences.Editor editor = sharedPreferences.edit();
+                    editor.putString("email",emailStr);
 
+                    editor.apply();// save changes
+
+                }
             }
         });
     }
